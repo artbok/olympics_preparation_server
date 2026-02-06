fbb7b5da733817e49a88b4fda44dd142ef41d80b
artbok <artem.bokans@gmail.com>
2026-02-06 10:24:24 +0300

    tasks services

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from routes.users import users_bp
-
+from routes.tasks import tasks_bp
 
 app = Flask(__name__)
 app.register_blueprint(users_bp)
-
+app.register_blueprint(tasks_bp)
     
 
 @app.after_request
 def add_headers(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type"
     return response
 
--- services/task_service.py
+++ services/task_service.py
@@ -2,10 +2,16 @@ from peewee import *
 from models.task import Task
 
 
-def createTask(subject, topic, difficulty, description, hint):
-    Task.create(subject = subject, topic = topic, difficulty = difficulty, description = description, hint = hint)
+def createTask(subject, topic, difficulty, description, hint, answer):
+    Task.create(subject = subject, topic = topic, difficulty = difficulty, description = description, hint = hint, answer = answer)
     
 
 def deleteTask(taskId):
     task: Task = Task.get(taskId)
     task.delete_instance()
+
+
+
+if not Task.table_exists():
+    Task.create_table()
+    print("Table 'Task' created")
